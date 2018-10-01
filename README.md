06e15638be04776691e4c791f408faa38ca34aa8
junyeongbluefin <=>
2018-10-01 05:58:52 -0400

    add peers

--- README.md
+++ README.md
@@ -29,6 +29,12 @@ Block hash algorithm
 Hash Validation
 
 
+PtoP Connection
+ - yarn dev
+ - HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 yarn dev
+ - HTTP_PORT=3003 P2P_PORT=5003 PEERS=ws://localhost:5002 yarn dev
+
+
 ## Test
  - yarn test
 
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,6 +1,6 @@
 const Websocket = require('ws');
 
-const P2P_PORT = process.evn.P2P_PORT || 5001;
+const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 class P2pServer {
@@ -12,7 +12,19 @@ class P2pServer {
     listen() {
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on('connection', socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
         console.log(`Listening for peer-to-peer connection on: ${P2P_PORT}`)
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+           // ws://localhost:5001
+           const socket = new Websocket(peer);
+
+           socket.on('open', () => this.connectSocket(socket));
+        });
     }
 
     connectSocket(socket) {
@@ -20,5 +32,7 @@ class P2pServer {
         console.log('Socket connected');
     }
 }
+
+module.exports = P2pServer;
 
 // $ HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001, ws://localhost:5002
