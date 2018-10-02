1465da572e3745c98e1f6b1f401c1e91e10b2c42
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2018-10-02 06:30:06 -0400

    Add public and private keys in Wallet

--- dev-test.js
+++ dev-test.js
@@ -1,11 +1,15 @@
-const Blockchain = require('./blockchain/blockchain');
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
+
+/*const Blockchain = require('./blockchain/blockchain');
 
 const bc = new Blockchain();
 
 for (let i = 0; i < 10; i++) {
     console.log(bc.addBlock(`foo ${i}`).toString());
 }
-
+*/
 
 //const Block = require('./block');
 
